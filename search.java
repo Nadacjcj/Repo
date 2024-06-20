4adf8f36a960c0d3cc53a04c653609dce1495ecc
Nadacjcj <nada1ehsan@gmail.com>
2024-06-20 16:13:57 +0300

    Mike added a new binary search method

--- search.java
+++ search.java
@@ -18,6 +18,31 @@ public class Search {
         return -1;
     }
 
+    public static void binarySearchCheckIfFound(int[] array, int target) {
+        Arrays.sort(array);
+
+        int left = 0;
+        int right = array.length - 1;
+        boolean flag = false;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (array[mid] == target) {
+                flag = true;
+                break;
+            } else if (array[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        if (!flag) {
+            System.out.println("Value " + target + " was not found using binary search");
+        } else {
+            System.out.println("Value " + target + " was found using binary search");
+        }
+    }
     public static void main(String[] args) {}
 
 }
