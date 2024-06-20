c98830178e9f1376628245f5efd336bd260537c0
Nadacjcj <nada1ehsan@gmail.com>
2024-06-20 15:13:26 +0300

    John updated search.java and added john_file1

--- search.java
+++ search.java
@@ -9,6 +9,14 @@ public class Search {
         }
         System.out.println("Value " + target + " was not found ");
     }
+    public static int linearSearchReturnIndex(int[] array, int target) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
 
     public static void main(String[] args) {}
 
